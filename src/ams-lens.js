67bd078f5005e2c0d6e644715146436f902ab37f
Peter Krautzberger <p.krautzberger@gmail.com>
2015-09-25 16:05:01 +0200

    Add AMS-specific functionality
    * add custom AMS info panel
    * add custom AMS cover view
    * add custom AMS publication info

--- src/ams-lens.js
+++ src/ams-lens.js
@@ -4,12 +4,30 @@ var _ = require('underscore');
 var Lens = require("lens/reader");
 var LensMath = require("lens/extensions/math");
 var MathConverter = LensMath.MathConverter;
-var MathNodes = LensMath.MathNodes;
 var MathPanel = LensMath.MathPanel;
+var MathNodes = LensMath.MathNodes;
 var panels = Lens.getDefaultPanels();
+var InfoPanel = require("./info_panel");
+var amsNodes = require("./nodes");
+
+// Add custom Nodes
+_.extend(MathNodes, amsNodes);
 
 // Insert math panel at next to last position
 panels.splice(1, 0, new MathPanel());
+
+var infoPanel = new InfoPanel({
+  type: 'resource',
+  name: 'info',
+  container: 'info',
+  title: 'Info',
+  icon: 'fa-info',
+  references: ['contributor_reference'],
+});
+
+// HACK: remove the old info panel, assuming at the last position
+panels.pop();
+panels.push(infoPanel);
 
 var AMSLens = function(config) {
   Lens.call(this, config);
