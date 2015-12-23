206f529d7c1966da3589dc7b770db7d1f7fec9c9
Peter Krautzberger <p.krautzberger@gmail.com>
2015-12-23 18:05:06 +0100

    load lens.js after MathJax.js; fixes #21

--- assets/lib/mjLoader.js
+++ assets/lib/mjLoader.js
@@ -1,18 +1,23 @@
 window.MathJax = {
-     jax: ["input/TeX", "input/MathML","output/HTML-CSS"],
-     extensions: ["MathMenu.js","MathZoom.js", "CHTML-preview.js"],
+    //  jax: ["input/TeX", "input/MathML","output/HTML-CSS"],
+    //  extensions: ["MathMenu.js","MathZoom.js", "CHTML-preview.js"],
      "HTML-CSS": { linebreaks: { automatic: true }, EqnChunk: 9999 },
      SVG: { linebreaks: { automatic: true }, EqnChunk: 9999  },
-     TeX: {
-       extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
-     },
+    //  extensions: ["[Contrib]/img.js"],
+    //  TeX: {extensions: ["AMSmath.js","AMSsymbols.js", "action.js"]},
      "displayAlign": "left",
-     "displayIndent": "5em" // TODO will often be article dependent. How do we implemet this?
+     "displayIndent": "5em", // TODO will often be article dependent. How do we implement this?
+     AuthorInit: function () {
+          // MathJax.Ajax.config.path["Contrib"] = "./assets/lib";
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.src = 'lens.js';
+          document.getElementsByTagName('head')[0].appendChild(script);
+      }
 };
 (function(d, script) {
   script = d.createElement('script');
   script.type = 'text/javascript';
-  script.async = true;
   script.onload = function() {
     // remote script has loaded
   };
