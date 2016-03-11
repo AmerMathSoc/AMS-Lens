dcbcd10156b1cdb683c30840d7ec8eb48d22ecfe
Peter Krautzberger <p.krautzberger@gmail.com>
2016-03-11 10:55:00 +0100

    [gulp] rewrite to recursively process directories

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 'use strict';
- 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserify = require('browserify');
@@ -7,20 +6,59 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var through2 = require('through2');
 var path = require('path');
+var argv = require('yargs').argv;
+var rename = require("gulp-rename");
+var replace = require('gulp-replace');
+var path = require('path');
+var fs = require('fs');
+var merge = require('merge-stream');
 
-gulp.task('assets', function() {
-  gulp.src('assets/**/*', {base:"./assets"})
-        .pipe(gulp.dest('dist'));
+// Usage: gulp -i pathname
+// Assumption: 1) pathname contains a bunch of folders 2) each of those child folders contain an XML file and possibly article assets
+// Result is written into the folder ./dist/[name]/
 
-  gulp.src('data/**/*', {base:"."})
-        .pipe(gulp.dest('dist'));
+var scriptsPath =  argv.i || './data'; //either pass a folder or build examples in `./data`
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+}
+
+gulp.task('dist', ['sass','browserify'], function() {
+   var folders = getFolders(scriptsPath);
+
+   var getAssets = folders.map(function(folder) {
+     console.log(folder);
+      return gulp.src(path.join(scriptsPath, folder)+'/**/*')
+        .pipe(gulp.dest(path.join('dist/', folder)));
+   });
+
+   var getLensAssets = folders.map(function(folder) {
+      return gulp.src('assets/**/*', {base:"./assets"})
+        .pipe(gulp.dest(path.join('dist/',folder)));
+   });
+
+   var getSass = folders.map(function(folder) {
+      return gulp.src('dist/lens.css')
+        .pipe(gulp.dest(path.join('dist/',folder)));
+   });
+
+    var getLens = folders.map(function(folder) {
+      var file = folder + '.xml';
+       return gulp.src('dist/lens.js')
+            .pipe(replace(/data\/arxiv-0312227\/arxiv-0312227.xml/g, file))
+            .pipe(gulp.dest(path.join('dist/',folder)));
+    });
+   return merge(getAssets, getLensAssets, getSass, getLens);
 });
 
 gulp.task('sass', function () {
   gulp.src('./lens.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('lens.css'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('browserify', function () {
@@ -39,7 +77,7 @@ gulp.task('browserify', function () {
         })
         .pipe(uglify())
         .pipe(rename('lens.js'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['assets', 'sass', 'browserify']);
+gulp.task('default', ['sass', 'browserify','dist']);
